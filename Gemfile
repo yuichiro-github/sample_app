35a9b0fdff2b95bf16f11250693fdfcc6f11b309
EC2 Default User <ec2-user@ip-172-31-3-126.ap-southeast-1.compute.internal>
2018-05-27 04:49:14 +0000

    Install the Bootstrap 3

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ ruby '2.4.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
+
+gem 'bootstrap-sass'
+gem 'sprockets'
+
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module SampleApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
