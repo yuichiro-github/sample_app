9fb6738871056f2b6c603dd6e64c1cae770b78f6
EC2 Default User <ec2-user@ip-172-31-3-126.ap-southeast-1.compute.internal>
2018-07-16 05:55:00 +0000

    Make a Mcropost model

---
+++ app/models/micropost.rb
@@ -0,0 +1,7 @@
+class Micropost < ApplicationRecord
+    belongs_to :user
+    default_scope -> { order('created_at DEXC') }
+    validates :content, presence: true, length: { maximum: 140 }
+    validates :user_id, presence: true
+    
+end
